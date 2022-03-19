929394d4765fa04ef675f608014f1f337a7bd2a2
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2022-03-19 16:24:04 -0300

    asd

--- src/lib/Layout.svelte
+++ src/lib/Layout.svelte
@@ -9,18 +9,15 @@
         </div>
     </section>
 
-    <section class="right">
-        <nav class="navigation">
-            <ul>
-                <li><a href="#hero">Inicio</a></li>
-                <li><a href="#services">Servicios</a></li>
-                <li><a href="#responsabilities">Responsabilidad</a></li>
-                <li><a href="#materials">Materiales</a></li>
-                <li><a href="#CV">CV</a></li>
-                <li><a href="#contact">Contacto</a></li>
-            </ul>
-        </nav>
-
+    <nav class="navigation">
+        <ul>
+            <li><a href="#hero">Inicio</a></li>
+            <li><a href="#services">Servicios</a></li>
+            <li><a href="#responsabilities">Responsabilidad</a></li>
+            <li><a href="#materials">Materiales</a></li>
+            <li><a href="#CV">CV</a></li>
+            <li><a href="#contact">Contacto</a></li>
+        </ul>
         <section class="phone-number">
             <span class="number-logo">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-mobile" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#5773ff" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -32,7 +29,18 @@
             </span>
             <p class="number">223 5 929292</p>
         </section>
-    </section>
+    </nav>
+
+    
+
+    <button class="burger">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <line x1="4" y1="6" x2="20" y2="6" />
+            <line x1="4" y1="12" x2="20" y2="12" />
+            <line x1="4" y1="18" x2="20" y2="18" />
+        </svg>
+    </button>
 </header>
 
 <main>
@@ -57,7 +65,7 @@
     }
 
     .app_header {
-        width:100vw;
+        max-width:100vw;
         height:12vh;
         background-color: #f5f5f5;
         display:flex;
@@ -85,9 +93,8 @@
 
     /* DANGER */
 
-    .right {
-        display: flex;
-        align-items: center;
+    .navigation {
+        display:flex;
     }
 
     .phone-number {
@@ -130,5 +137,38 @@
         text-decoration: none;
     }
 
+    .burger {
+        border:none;
+        background:none;
+        outline:none;
+        cursor:pointer;
+        display:none;
+    }
 
+    @media screen and (max-width:1200px) {
+        .app_header {
+            max-width: 100vw;
+            justify-content: space-between;
+        }
+
+        .content {
+            margin-left: 2rem;
+        }
+
+        .burger {
+            display:flex;
+            padding:0 4rem;
+        }
+
+        .navigation {
+            position:fixed;
+            top:12vh;
+            left:0;
+            display:block;
+        }
+
+        ul {
+            flex-direction: column;
+        }
+    }
 </style>
